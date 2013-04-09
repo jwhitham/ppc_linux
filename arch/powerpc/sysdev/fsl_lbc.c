0d802887356cdd328d57bf41d9e9218d18f45310
Wood Scott-B07421 <scottwood@freescale.com>
2013-04-09 18:25:59 -0500

    Revert "powerpc/85xx: handle the eLBC error interrupt if it exist in dts"
    This reverts commit 7c7fa5af76cc71afe92289dfd82cbee49e210b6c
    
    In sdk-1.3.x this was reverted by d564e68e5b01c30bed70a63c5b63d04491b84caf
    
    Change-Id: I324777a0a146021b3dab6d5209c23e7ee731171a
    Reviewed-on: http://git.am.freescale.net:8181/1182
    Reviewed-by: Schmitt Richard-B43082 <B43082@freescale.com>
    Reviewed-by: Fleming Andrew-AFLEMING <AFLEMING@freescale.com>
    Tested-by: Fleming Andrew-AFLEMING <AFLEMING@freescale.com>

--- arch/powerpc/sysdev/fsl_lbc.c
+++ arch/powerpc/sysdev/fsl_lbc.c
@@ -48,8 +48,7 @@ u32 fsl_lbc_addr(phys_addr_t addr_base)
 	struct device_node *np = fsl_lbc_ctrl_dev->dev->of_node;
 	u32 addr = addr_base & 0xffff8000;
 
-	if (of_device_is_compatible(np, "fsl,elbc") ||
-			of_device_is_compatible(np, "fsl,p3041-rev1.0-elbc"))
+	if (of_device_is_compatible(np, "fsl,elbc"))
 		return addr;
 
 	return addr | ((addr_base & 0x300000000ull) >> 19);
@@ -199,8 +198,7 @@ static int fsl_lbc_ctrl_init(struct fsl_lbc_ctrl *ctrl,
 	out_be32(&lbc->ltedr, LTEDR_ENABLE);
 
 	/* Set the monitor timeout value to the maximum for erratum A001 */
-	if (of_device_is_compatible(node, "fsl,elbc") ||
-			of_device_is_compatible(node, "fsl,p3041-rev1.0-elbc"))
+	if (of_device_is_compatible(node, "fsl,elbc"))
 		clrsetbits_be32(&lbc->lbcr, LBCR_BMT, LBCR_BMTPS);
 
 	return 0;
@@ -216,14 +214,10 @@ static irqreturn_t fsl_lbc_ctrl_irq(int irqno, void *data)
 	struct fsl_lbc_ctrl *ctrl = data;
 	struct fsl_lbc_regs __iomem *lbc = ctrl->regs;
 	u32 status;
-	unsigned long flags;
 
-	spin_lock_irqsave(&fsl_lbc_lock, flags);
 	status = in_be32(&lbc->ltesr);
-	if (!status) {
-		spin_unlock_irqrestore(&fsl_lbc_lock, flags);
+	if (!status)
 		return IRQ_NONE;
-	}
 
 	out_be32(&lbc->ltesr, LTESR_CLEAR);
 	out_be32(&lbc->lteatr, 0);
@@ -266,7 +260,6 @@ static irqreturn_t fsl_lbc_ctrl_irq(int irqno, void *data)
 	if (status & ~LTESR_MASK)
 		dev_err(ctrl->dev, "Unknown error: "
 			"LTESR 0x%08X\n", status);
-	spin_unlock_irqrestore(&fsl_lbc_lock, flags);
 	return IRQ_HANDLED;
 }
 
@@ -305,8 +298,8 @@ static int fsl_lbc_ctrl_probe(struct platform_device *dev)
 		goto err;
 	}
 
-	fsl_lbc_ctrl_dev->irq[0] = irq_of_parse_and_map(dev->dev.of_node, 0);
-	if (fsl_lbc_ctrl_dev->irq[0] == NO_IRQ) {
+	fsl_lbc_ctrl_dev->irq = irq_of_parse_and_map(dev->dev.of_node, 0);
+	if (fsl_lbc_ctrl_dev->irq == NO_IRQ) {
 		dev_err(&dev->dev, "failed to get irq resource\n");
 		ret = -ENODEV;
 		goto err;
@@ -318,25 +311,13 @@ static int fsl_lbc_ctrl_probe(struct platform_device *dev)
 	if (ret < 0)
 		goto err;
 
-	ret = request_irq(fsl_lbc_ctrl_dev->irq[0], fsl_lbc_ctrl_irq, 0,
+	ret = request_irq(fsl_lbc_ctrl_dev->irq, fsl_lbc_ctrl_irq, 0,
 				"fsl-lbc", fsl_lbc_ctrl_dev);
 	if (ret != 0) {
 		dev_err(&dev->dev, "failed to install irq (%d)\n",
-			fsl_lbc_ctrl_dev->irq[0]);
-		ret = fsl_lbc_ctrl_dev->irq[0];
+			fsl_lbc_ctrl_dev->irq);
+		ret = fsl_lbc_ctrl_dev->irq;
 		goto err;
-	}
-
-	fsl_lbc_ctrl_dev->irq[1] = irq_of_parse_and_map(dev->dev.of_node, 1);
-	if (fsl_lbc_ctrl_dev->irq[1] != NO_IRQ) {
-		ret = request_irq(fsl_lbc_ctrl_dev->irq[1], fsl_lbc_ctrl_irq,
-				IRQF_SHARED, "fsl-lbc-err", fsl_lbc_ctrl_dev);
-		if (ret != 0) {
-			dev_err(&dev->dev, "failed to install irq (%d)\n",
-					fsl_lbc_ctrl_dev->irq[1]);
-			ret = fsl_lbc_ctrl_dev->irq[1];
-			goto err;
-		}
 	}
 
 	/* Enable interrupts for any detected events */
@@ -385,11 +366,6 @@ static int fsl_lbc_resume(struct platform_device *pdev)
 
 static const struct of_device_id fsl_lbc_match[] = {
 	{ .compatible = "fsl,elbc", },
-	{ .compatible = "fsl,p2040-rev1.0-elbc", },
-	{ .compatible = "fsl,p2041-rev1.0-elbc", },
-	{ .compatible = "fsl,p3041-rev1.0-elbc", },
-	{ .compatible = "fsl,p5010-rev1.0-elbc", },
-	{ .compatible = "fsl,p5020-rev1.0-elbc", },
 	{ .compatible = "fsl,pq3-localbus", },
 	{ .compatible = "fsl,pq2-localbus", },
 	{ .compatible = "fsl,pq2pro-localbus", },
