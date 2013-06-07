0a899f8c2acd8e217d24933d6261ad3fb0a21e26
Madalin Bucur <madalin.bucur@freescale.com>
2013-06-07 17:18:50 +0300

    dpaa_eth: add shared and MACless compatibility strings
    Add dedicated compatibility strings for shared and MACless
    interfaces: "fsl,dpa-ethernet-shared" and "fsl,dpa-ethernet-macless".
    
    Signed-off-by: Madalin Bucur <madalin.bucur@freescale.com>
    Change-Id: I71a9f14e53e9ecbbba5f837589708cc847b41e4e
    Reviewed-on: http://git.am.freescale.net:8181/3080
    Reviewed-by: Sovaiala Cristian-Constantin-B39531 <Cristian.Sovaiala@freescale.com>
    Reviewed-by: Radulescu Ruxandra Ioana-B05472 <ruxandra.radulescu@freescale.com>
    Reviewed-by: Fleming Andrew-AFLEMING <AFLEMING@freescale.com>
    Tested-by: Fleming Andrew-AFLEMING <AFLEMING@freescale.com>

--- drivers/net/ethernet/freescale/dpa/dpaa_eth.c
+++ drivers/net/ethernet/freescale/dpa/dpaa_eth.c
@@ -4320,7 +4320,9 @@ dpaa_eth_proxy_probe(struct platform_device *_of_dev)
 
 static const struct of_device_id dpa_match[] = {
 	{
-		.compatible	= "fsl,dpa-ethernet"
+		.compatible	= "fsl,dpa-ethernet",
+		.compatible	= "fsl,dpa-ethernet-shared",
+		.compatible	= "fsl,dpa-ethernet-macless"
 	},
 	{}
 };
