6b7db5dc558dcf99f781be85a688abcdf75f2869
Claudiu Manoil <claudiu.manoil@freescale.com>
2013-07-30 14:36:49 +0300

    gianfar: Add ethtool -A support for pause frame
    Allow Rx/Tx pause frame configuration via ethtool.
    The gfar devices feature link autonegotioation by default.
    The device is being configured with the new pause frame
    settings during link autonegotiation (see adjust_link()).
    
    Signed-off-by: Claudiu Manoil <claudiu.manoil@freescale.com>
    Change-Id: Icd0b4155ad10e6f9174ffe17c8bf5bec748f9142
    Reviewed-on: http://git.am.freescale.net:8181/3651
    Reviewed-by: Bucur Madalin-Cristian-B32716 <madalin.bucur@freescale.com>
    Reviewed-by: Fleming Andrew-AFLEMING <AFLEMING@freescale.com>
    Tested-by: Fleming Andrew-AFLEMING <AFLEMING@freescale.com>

--- drivers/net/ethernet/freescale/gianfar.h
+++ drivers/net/ethernet/freescale/gianfar.h
@@ -1446,6 +1446,7 @@ void gfar_init_sysfs(struct net_device *dev);
 int gfar_set_features(struct net_device *dev, netdev_features_t features);
 extern void gfar_check_rx_parser_mode(struct gfar_private *priv);
 extern void gfar_vlan_mode(struct net_device *dev, netdev_features_t features);
+void gfar_configure_pause(struct gfar_private *priv, bool en);
 
 extern const struct ethtool_ops gfar_ethtool_ops;
 extern struct list_head gfar_recycle_queues;
