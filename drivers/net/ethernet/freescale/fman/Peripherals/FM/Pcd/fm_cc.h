605d2ad9a561593fa1187b72568fc9b353815a2f
Mandy Lavi <mandy.lavi@freescale.com>
2013-07-30 14:26:03 +0300

    fmd: support retrieval of more statistics counters
    - miss entry in a CC Node
    - miss entry in the hash table
    
    Signed-off-by: Mandy Lavi <mandy.lavi@freescale.com>
    Change-Id: Iacbebff6f8f7ef1fa9c97b24d1072515f5098860
    Reviewed-on: http://git.am.freescale.net:8181/3828
    Reviewed-by: Lavi Mandy-R52568 <Mandy.Lavi@freescale.com>
    Reviewed-by: Chereji Marian-Cornel-R27762 <marian.chereji@freescale.com>
    Reviewed-by: Schmitt Richard-B43082 <B43082@freescale.com>
    Tested-by: Schmitt Richard-B43082 <B43082@freescale.com>

--- drivers/net/ethernet/freescale/fman/Peripherals/FM/Pcd/fm_cc.h
+++ drivers/net/ethernet/freescale/fman/Peripherals/FM/Pcd/fm_cc.h
@@ -310,6 +310,17 @@ typedef struct
     uint32_t            numOfStatsFLRs;
     uint32_t            countersArraySize;
 
+    bool                isHashBucket;               /**< Valid for match table node that is a bucket of a hash table only */
+    t_Handle            h_MissStatsCounters;        /**< Valid for hash table node and match table that is a bucket;
+                                                         Holds the statistics counters allocated by the hash table and
+                                                         are shared by all hash table buckets; */
+    t_Handle            h_PrivMissStatsCounters;    /**< Valid for match table node that is a bucket of a hash table only;
+                                                         Holds the statistics counters that were allocated for this node
+                                                         and replaced by the shared counters (allocated by the hash table); */
+    bool                statsEnForMiss;             /**< Valid for hash table node only; TRUE is statistics are currently
+                                                         enabled for hash 'miss', FALSE otherwise; This parameter effects the
+                                                         returned statistics count to user, statistics AD always present for 'miss'
+                                                         for all hash buckets; */
     bool                glblMaskUpdated;
     t_Handle            p_GlblMask;
     bool                lclMask;
