2eeeeda42104eec74cd1dc40f4d4da3442994614
Jingchang Lu <b35083@freescale.com>
2013-09-16 10:01:28 +0800

    ARM: imx: Add initial support for Freescale LS1021A
    The LS1021A SoC is a dual-core Cortex-A7 based processor,
    this add the initial support for it.
    
    Signed-off-by: Jingchang Lu <b35083@freescale.com>
    ---
    This patch has been sent to upstream for review:
    https://patchwork.kernel.org/patch/4464451/

---
+++ arch/arm/mach-imx/mach-ls1021a.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2013-2014 Freescale Semiconductor, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <linux/of_platform.h>
+#include <asm/mach/arch.h>
+
+#include "common.h"
+
+static void __init ls1021a_init_machine(void)
+{
+	mxc_arch_reset_init_dt();
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+}
+
+static const char *ls1021a_dt_compat[] __initdata = {
+	"fsl,ls1021a",
+	NULL,
+};
+
+DT_MACHINE_START(LS1021A, "Freescale LS1021A")
+#ifdef CONFIG_ZONE_DMA
+	.dma_zone_size	= SZ_128M,
+#endif
+	.init_machine   = ls1021a_init_machine,
+	.dt_compat	= ls1021a_dt_compat,
+	.restart	= mxc_restart,
+MACHINE_END
