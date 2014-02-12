e3209a1ab89d69e835391f14de71e44e93b7f41c
Horia Geanta <horia.geanta@freescale.com>
2014-02-12 16:20:06 +0200

    xfrm: avoid creating temporary SA when there are no listeners
    In the case when KMs have no listeners, km_query() will fail and
    temporary SAs are garbage collected immediately after their allocation.
    This causes strain on memory allocation, leading even to OOM since
    temporary SA alloc/free cycle is performed for every packet
    and garbage collection does not keep up the pace.
    
    The sane thing to do is to make sure we have audience before
    temporary SA allocation.
    
    Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Change-Id: I241fdd8133f4974aaf14a9bc12be089aaa1730ae
    Reviewed-on: http://git.am.freescale.net:8181/12383
    Reviewed-by: Marian Cristian Rotariu <marian.rotariu@freescale.com>
    Reviewed-by: Jose Rivera <German.Rivera@freescale.com>
    Tested-by: Jose Rivera <German.Rivera@freescale.com>

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -2987,6 +2987,11 @@ static int xfrm_send_mapping(struct xfrm_state *x, xfrm_address_t *ipaddr,
 	return nlmsg_multicast(net->xfrm.nlsk, skb, 0, XFRMNLGRP_MAPPING, GFP_ATOMIC);
 }
 
+static bool xfrm_is_alive(const struct km_event *c)
+{
+	return (bool)xfrm_acquire_is_on(c->net);
+}
+
 static struct xfrm_mgr netlink_mgr = {
 	.id		= "netlink",
 	.notify		= xfrm_send_state_notify,
@@ -2996,6 +3001,7 @@ static struct xfrm_mgr netlink_mgr = {
 	.report		= xfrm_send_report,
 	.migrate	= xfrm_send_migrate,
 	.new_mapping	= xfrm_send_mapping,
+	.is_alive	= xfrm_is_alive,
 };
 
 static int __net_init xfrm_user_net_init(struct net *net)
