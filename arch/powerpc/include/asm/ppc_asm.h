e69d7cf496b4a7f6dfb0c7c9acda1797c2a9e608
Scott Wood <scottwood@freescale.com>
2014-03-25 19:24:57 -0500

    Revert "powerpc/mpc85xx/e6500: work around CPU erratum A-006198"
    This reverts commit f4c0e693ccc3422c5b809e7cc8f59b7637e3b7ab.
    
    rev1 is no longer supported in the SDK, and the workaround for A-006198
    conflicts with machine check support.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Change-Id: I64e1ce19eb59a6bba8649156597cc49ff9b62b1e
    Reviewed-on: http://git.am.freescale.net:8181/10271
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Kim Phillips <Kim.Phillips@freescale.com>
    Reviewed-by: Jose Rivera <German.Rivera@freescale.com>

--- arch/powerpc/include/asm/ppc_asm.h
+++ arch/powerpc/include/asm/ppc_asm.h
@@ -755,49 +755,6 @@ END_FTR_SECTION_NESTED(CPU_FTR_HAS_PPR,CPU_FTR_HAS_PPR,945)
 #define N_SLINE	68
 #define N_SO	100
 
-.macro fsl_erratum_a006198_mtmsr newmsr scratch1 scratch2
-#ifdef CONFIG_FSL_ERRATUM_A_006198
-	mflr	\scratch2
-	LOAD_REG_IMMEDIATE(\scratch1, 237f)
-	mtlr	\scratch1
-	LOAD_REG_IMMEDIATE(\scratch1, .fsl_erratum_a006198_return)
-	mtspr	SPRN_MCSRR1, \newmsr
-	mtspr	SPRN_MCSRR0, \scratch1
-	rfmci
-237:	mtmsr	\newmsr
-	mtlr	\scratch2
-#else
-	mtmsr	\newmsr
-#endif
-.endm
-
-.macro fsl_erratum_a006198_wrteei0 scratch1 scratch2
-#ifdef CONFIG_FSL_ERRATUM_A_006198
-	mflr	\scratch2
-	LOAD_REG_IMMEDIATE(\scratch1, 237f)
-	mtlr	\scratch1
-	LOAD_REG_IMMEDIATE(\scratch1, .fsl_erratum_a006198_return)
-	mtspr	SPRN_MCSRR0, \scratch1
-	mfmsr	\scratch1
-	rlwinm	\scratch1, \scratch1, 0, ~MSR_EE
-	mtspr	SPRN_MCSRR1, \scratch1
-	rfmci
-237:	mtmsr	\scratch1
-	mtlr	\scratch2
-#else
-	wrteei	0
-#endif
-.endm
-
-.macro fsl_erratum_a006198_restore_srr scratch
-#ifdef CONFIG_FSL_ERRATUM_A_006198
-	LOAD_REG_IMMEDIATE(\scratch, .fsl_erratum_a006198_return)
-	mtspr	SPRN_MCSRR0, \scratch
-	lis	\scratch, MSR_CM@h
-	mtspr	SPRN_MCSRR1, \scratch
-#endif
-.endm
-
 /*
  * Create an endian fixup trampoline
  *
