47a0ea3dc8b50056f8dd39afb438d362d5f5ea45
Tudor Ambarus <tudor.ambarus@freescale.com>
2014-04-03 14:12:21 +0300

    crypto: caam - fix key typo in gcm descriptors
    There is only one key for the aes gcm algorithm.
    
    Change-Id: I1f53edf41319083aa34ac9b948bc35c3d7886dde
    Signed-off-by: Tudor Ambarus <tudor.ambarus@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/10816
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Horia Ioan Geanta Neag <horia.geanta@freescale.com>
    Reviewed-by: Jose Rivera <German.Rivera@freescale.com>

--- drivers/crypto/caam/caamalg.c
+++ drivers/crypto/caam/caamalg.c
@@ -928,7 +928,7 @@ static int gcm_set_sh_desc(struct crypto_aead *aead)
 	struct aead_tfm *tfm = &aead->base.crt_aead;
 	struct caam_ctx *ctx = crypto_aead_ctx(aead);
 	struct device *jrdev = ctx->jrdev;
-	bool keys_fit_inline = false;
+	bool key_fits_inline = false;
 	u32 *key_jump_cmd, *zero_payload_jump_cmd,
 	    *zero_assoc_jump_cmd1, *zero_assoc_jump_cmd2;
 	u32 *desc;
@@ -943,16 +943,16 @@ static int gcm_set_sh_desc(struct crypto_aead *aead)
 	 */
 	if (DESC_GCM_ENC_LEN + DESC_JOB_IO_LEN +
 	    ctx->enckeylen <= CAAM_DESC_BYTES_MAX)
-		keys_fit_inline = true;
+		key_fits_inline = true;
 
 	desc = ctx->sh_desc_enc;
 
 	init_sh_desc(desc, HDR_SHARE_SERIAL);
 
-	/* skip key loading if they are loaded due to sharing */
+	/* skip key loading if it is loaded due to sharing */
 	key_jump_cmd = append_jump(desc, JUMP_JSL | JUMP_TEST_ALL |
 				   JUMP_COND_SHRD);
-	if (keys_fit_inline)
+	if (key_fits_inline)
 		append_key_as_imm(desc, (void *)ctx->key, ctx->enckeylen,
 				  ctx->enckeylen, CLASS_1 | KEY_DEST_CLASS_REG);
 	else
@@ -1050,16 +1050,16 @@ static int gcm_set_sh_desc(struct crypto_aead *aead)
 	 */
 	if (DESC_GCM_DEC_LEN + DESC_JOB_IO_LEN +
 	    ctx->enckeylen <= CAAM_DESC_BYTES_MAX)
-		keys_fit_inline = true;
+		key_fits_inline = true;
 
 	desc = ctx->sh_desc_dec;
 
 	init_sh_desc(desc, HDR_SHARE_SERIAL);
 
-	/* skip key loading if they are loaded due to sharing */
+	/* skip key loading if it is loaded due to sharing */
 	key_jump_cmd = append_jump(desc, JUMP_JSL | JUMP_TEST_ALL |
 				   JUMP_COND_SHRD);
-	if (keys_fit_inline)
+	if (key_fits_inline)
 		append_key_as_imm(desc, (void *)ctx->key, ctx->enckeylen,
 				  ctx->enckeylen, CLASS_1 | KEY_DEST_CLASS_REG);
 	else
