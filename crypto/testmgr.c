d94cac7c7df1cd82be57150e8d99054058646779
Cristian Stoica <cristian.stoica@freescale.com>
2014-04-24 11:57:11 +0300

    testmgr: fix TLS module build error
    This is a fix specific for sdk-v1.6.x with linux kernel 3.12.
    reinit_completion does not exist yet - it was introduced shortly after kernel 3.12.
    
    Signed-off-by: Cristian Stoica <cristian.stoica@freescale.com>
    Change-Id: Ic154ed5c64ac9f3c5cfeefd6ff0c180e8b543216
    Reviewed-on: http://git.am.freescale.net:8181/11447
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Horia Ioan Geanta Neag <horia.geanta@freescale.com>
    Reviewed-by: Madalin-Cristian Bucur <madalin.bucur@freescale.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -931,7 +931,7 @@ static int __test_tls(struct crypto_aead *tfm, int enc,
 			ret = wait_for_completion_interruptible(
 						&result.completion);
 			if (!ret && !result.err) {
-				reinit_completion(&result.completion);
+				INIT_COMPLETION(result.completion);
 				break;
 			} else {
 				ret = result.err;
