dcdf3c4866f92abf960dd4b9a5be1aaed4887405
Wang Dongsheng <dongsheng.wang@freescale.com>
2015-03-20 13:45:24 +0800

    fsl: Use ccflags-y instead of deprecated EXTRA_CFLAGS
    This allows users to pass in additional compiler flags through the
    environment variable EXTRA_CFLAGS, e.g.
    
       make EXTRA_CFLAGS=-Wno-error
    
    Signed-off-by: Wang Dongsheng <dongsheng.wang@freescale.com>
    Change-Id: I36513506ee1671c31bb076e9b73c2e8e2442cf4f
    Reviewed-on: http://git.am.freescale.net:8181/33146
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Honghua Yin <Hong-Hua.Yin@freescale.com>

--- drivers/net/ethernet/freescale/fman/ncsw_config.mk
+++ drivers/net/ethernet/freescale/fman/ncsw_config.mk
@@ -6,36 +6,36 @@ DRV_DPA     = $(srctree)/drivers/net/ethernet/freescale/dpa
 FMAN        = $(srctree)/drivers/net/ethernet/freescale/fman
 
 ifeq ("$(CONFIG_FMAN_P3040_P4080_P5020)", "y")
-EXTRA_CFLAGS +=-include $(FMAN)/p3040_4080_5020_dflags.h
+ccflags-y +=-include $(FMAN)/p3040_4080_5020_dflags.h
 endif
 ifeq ("$(CONFIG_FMAN_P1023)", "y")
-EXTRA_CFLAGS +=-include $(FMAN)/p1023_dflags.h
+ccflags-y +=-include $(FMAN)/p1023_dflags.h
 endif
 ifdef CONFIG_FMAN_T4240
-EXTRA_CFLAGS +=-include $(FMAN)/t4240_dflags.h
+ccflags-y +=-include $(FMAN)/t4240_dflags.h
 endif
 
-EXTRA_CFLAGS += -I$(DRV_DPA)/
-EXTRA_CFLAGS += -I$(FMAN)/inc
-EXTRA_CFLAGS += -I$(FMAN)/inc/cores
-EXTRA_CFLAGS += -I$(FMAN)/inc/etc
-EXTRA_CFLAGS += -I$(FMAN)/inc/Peripherals
-EXTRA_CFLAGS += -I$(FMAN)/inc/flib
+ccflags-y += -I$(DRV_DPA)/
+ccflags-y += -I$(FMAN)/inc
+ccflags-y += -I$(FMAN)/inc/cores
+ccflags-y += -I$(FMAN)/inc/etc
+ccflags-y += -I$(FMAN)/inc/Peripherals
+ccflags-y += -I$(FMAN)/inc/flib
 
 ifeq ("$(CONFIG_FMAN_P3040_P4080_P5020)", "y")
-EXTRA_CFLAGS += -I$(FMAN)/inc/integrations/P3040_P4080_P5020
+ccflags-y += -I$(FMAN)/inc/integrations/P3040_P4080_P5020
 endif
 ifeq ("$(CONFIG_FMAN_P1023)", "y")
-EXTRA_CFLAGS += -I$(FMAN)/inc/integrations/P1023
+ccflags-y += -I$(FMAN)/inc/integrations/P1023
 endif
 ifdef CONFIG_FMAN_T4240
-EXTRA_CFLAGS += -I$(FMAN)/inc/integrations/T4240
+ccflags-y += -I$(FMAN)/inc/integrations/T4240
 endif
 
-EXTRA_CFLAGS += -I$(FMAN)/src/inc
-EXTRA_CFLAGS += -I$(FMAN)/src/inc/system
-EXTRA_CFLAGS += -I$(FMAN)/src/inc/wrapper
-EXTRA_CFLAGS += -I$(FMAN)/src/inc/xx
-EXTRA_CFLAGS += -I$(srctree)/include/uapi/linux/fmd
-EXTRA_CFLAGS += -I$(srctree)/include/uapi/linux/fmd/Peripherals
-EXTRA_CFLAGS += -I$(srctree)/include/uapi/linux/fmd/integrations
+ccflags-y += -I$(FMAN)/src/inc
+ccflags-y += -I$(FMAN)/src/inc/system
+ccflags-y += -I$(FMAN)/src/inc/wrapper
+ccflags-y += -I$(FMAN)/src/inc/xx
+ccflags-y += -I$(srctree)/include/uapi/linux/fmd
+ccflags-y += -I$(srctree)/include/uapi/linux/fmd/Peripherals
+ccflags-y += -I$(srctree)/include/uapi/linux/fmd/integrations
